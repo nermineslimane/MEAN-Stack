9090d8ec3aa6d9bffc0246f7d4fc567cef80bb19
nermineslimane <slimane.nermine@esprit.tn>
2020-11-07 13:08:46 +0100

    basics of angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { PostCreateComponent } from './Posts/Post-create/post-create.comoponent';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
