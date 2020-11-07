58a2312ac7940db7e2d7087aeba2bdbda4cce2dd
nermineslimane <slimane.nermine@esprit.tn>
2020-11-07 14:00:48 +0100

    installing and using angular material

--- src/app/Posts/Post-create/post-create.comoponent.ts
+++ src/app/Posts/Post-create/post-create.comoponent.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.comoponent.css']
 })
 export class PostCreateComponent{
 newPost="";
 entredValue=""
+
 onAddPost(){
 
 this.newPost=this.entredValue
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input'
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
+const modules = [
+  //MatButtonModule,
+  //MatFormFieldModule,
+   //MatRippleModule,
+  MatInputModule,
+  BrowserModule,
+  FormsModule,
+  BrowserAnimationsModule,
+  MatCardModule,
+  MatButtonModule
+
+];
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  imports: [...modules],
   providers: [],
   bootstrap: [AppComponent]
 })
